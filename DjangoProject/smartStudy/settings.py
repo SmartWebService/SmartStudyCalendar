0a877b5c8f193c4bbfa85ac3dd2ac0e215af1bf0
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2019-10-18 12:43:13 +0900

    헤로쿠테스트

--- DjangoProject/smartStudy/settings.py
+++ DjangoProject/smartStudy/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 ) 
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
